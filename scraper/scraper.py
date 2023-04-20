46e37e7e13c87bae18f594790636f442e84db0ef
Miguel Salazar <74027257+miguelsalazar88@users.noreply.github.com>
2023-04-20 11:35:27 -0500

    Synchronizing job scraper.py from AWS Glue at 2023/04/20 16:35:27

---
+++ scraper/scraper.py
@@ -0,0 +1,26 @@
+import sys
+import boto3
+import requests
+from datetime import datetime
+
+
+s3 = boto3.client('s3')
+urls = ['https://www.eltiempo.com/', 
+        'https://www.elespectador.com/']
+
+newspapers = ['el_tiempo', 'el_espectador']
+
+# Fecha actual
+now = datetime.now()
+date = now.strftime('%Y-%m-%d')
+
+for url, newspaper in zip(urls, newspapers):
+    response = requests.get(url)
+    html_content = response.text
+    s3.put_object(
+    Bucket='headlines-raw',
+    Key=f'{newspaper}/contenido-{date}.html',
+    Body= html_content
+)
+
+
